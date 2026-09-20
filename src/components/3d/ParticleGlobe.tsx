7030b8c7d8ea3fb6218059dc27fde6c5b9658daf
Aakash Bhatt <aakashbhatt076@gmail.com>
2026-09-20 12:19:11 +0530

    fix(build): add missing args to bufferAttribute in ParticleGlobe

--- src/components/3d/ParticleGlobe.tsx
+++ src/components/3d/ParticleGlobe.tsx
@@ -40,6 +40,7 @@ export default function ParticleGlobe() {
       <bufferGeometry>
         <bufferAttribute
           attach="attributes-position"
+          args={[positions, 3]}
           count={positions.length / 3}
           array={positions}
           itemSize={3}
